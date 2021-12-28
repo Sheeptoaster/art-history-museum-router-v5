c6512417f3ee2178266b47fddca8753677a2c85a
nathaniel <nathanieltseng@gmail.com>
2021-12-28 13:49:04 -0800

    phase 3

--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,14 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, Redirect } from "react-router-dom";
 
 function GalleryView({ galleries }) {
 	const { galleryId } = useParams();
 	const thisGallery = galleries.find(gallery => gallery.galleryid === parseInt(galleryId));
-	return (
-		<>
-			<h2>{thisGallery.name}</h2>
-			<div>{thisGallery.labeltext}</div>
-		</>
-	)
+	if (thisGallery) {
+		return (
+			<>
+				<h2>{thisGallery.name}</h2>
+				<div>{thisGallery.labeltext}</div>
+			</>
+		)
+	} else {
+		return (
+			<>
+				<Redirect to='/a;efioawe'></Redirect>
+			</>
+		)
+	}
 }
 
 export default GalleryView;
